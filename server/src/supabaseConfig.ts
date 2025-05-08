acd9b1267183fceace8e57257e6cba604b904fe0
arthurfelipe3252 <arthurfelipebalmantdesouza2019@gmail.com>
2025-05-08 05:33:52 -0300

    Implementa storage

---
+++ server/src/supabaseConfig.ts
@@ -0,0 +1,16 @@
+
+import { createClient } from '@supabase/supabase-js';
+import {SUPABASEURL, SUPABASEKEY,} from './config'
+
+
+const supabaseUrl = SUPABASEURL!;
+const supabaseKey = SUPABASEKEY!;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+
+export const getPublicUrl = (bucket: string, path: string) => {
+  const { data } = supabase.storage.from(bucket).getPublicUrl(path);
+  return data.publicUrl;
+};
