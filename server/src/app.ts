95cfdee1adb924734954b2f25d805c169303e0e2
arthurfelipe3252 <arthurfelipebalmantdesouza2019@gmail.com>
2025-05-08 06:35:22 -0300

    fix: Correção de login

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,8 @@ import allRoutes from './routes/appRoutes';
 const app = express();
 const routes = allRoutes;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+
 app.use('/', routes);
 
 export default app;
