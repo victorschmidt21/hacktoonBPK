d7f1de528827a66b6364e09c307d233307c302e3
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-05-08 06:08:41 -0300

    fix: fix infos to login

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import allRoutes from './routes/appRoutes';
 
 const app = express();
 const routes = allRoutes;
 
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/', routes);
 
--- server/src/routes/appRoutes.ts
+++ server/src/routes/appRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usrRoute from './users/userRoutes'
+import eventRoutes from './events/eventRoutes';
 
 const allRoutes = Router();
 
 allRoutes.use('/user', usrRoute);
+allRoutes.use('/evento', eventRoutes);
 
 export default allRoutes;
