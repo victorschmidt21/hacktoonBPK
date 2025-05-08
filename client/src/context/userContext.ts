0ba448febb44eaa7aac7007da0679c5572cff1aa
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-05-08 03:34:02 -0300

    fix: comments userContext

--- client/src/context/userContext.ts
+++ client/src/context/userContext.ts
@@ -1,14 +1,14 @@
-// import { create } from 'zustand';
-// import type { UserAttributes } from '../api/routes/user/user';
+import { create } from 'zustand';
+import type { UserAttributes } from '../api/routes/user/user';
 
-// interface UserState {
-//   user: UserAttributes | null;
-//   setUser: (user: UserAttributes) => void;
-//   clearUser: () => void;
-// }
+interface UserState {
+  user: UserAttributes | null;
+  setUser: (user: UserAttributes) => void;
+  clearUser: () => void;
+}
 
-// export const useUserStore = create<UserState>((set) => ({
-//   user: null,
-//   setUser: (user) => set({ user }),
-//   clearUser: () => set({ user: null }),
-// }));
+export const useUserStore = create<UserState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+}));
